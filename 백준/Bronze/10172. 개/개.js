37bbdb417a070189dccb826ae55987f94c4b703e
Yunjoo Cho <zooyaa@kakao.com>
2024-10-27 18:01:07 +0900

    [Bronze V] Title: 개, Time: 92 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+function solution() {
+    console.log("|\\_/|");
+    console.log("|q p|   /}");
+    console.log("( 0 )\"\"\"\\");
+    console.log("|\"^\"`    |");
+    console.log("||_/=\\\\__|");
+}
+
+solution();
