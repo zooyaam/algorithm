f91f0fa2937983cb46fd20bb01f2cf02a96d600c
Yunjoo Cho <zooyaa@kakao.com>
2025-02-09 12:49:21 +0900

    [level 0] Title: 로그인 성공?, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,7 +1,8 @@
 function solution(id_pw, db) {
-    return db.some(data => data[0] === id_pw[0] && data[1] !== id_pw[1])
-        ? 'wrong pw'
-        : db.some(data => data[0] === id_pw[0] && data[1] === id_pw[1])
-            ? 'login'
-            : 'fail'
+    for(const [id, pw] of db) {
+        if(id_pw[0] === id) {
+            return id_pw[1] === pw ? 'login' : 'wrong pw';
+        }
+    }
+    return 'fail';
 }
