d77fcb280a487e8c4d48ae4ab333689f1158bec1
Yunjoo Cho <zooyaa@kakao.com>
2025-04-10 16:53:54 +0900

    [Bronze III] Title: Arm Coordination, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/23375. Arm Coordination/Arm Coordination.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [x, y] = input[0].split(' ').map(Number);
+const r = Number(input[1]);
+
+console.log(`${x - r} ${y - r}`);
+console.log(`${x - r} ${y + r}`);
+console.log(`${x + r} ${y + r}`);
+console.log(`${x + r} ${y - r}`);
