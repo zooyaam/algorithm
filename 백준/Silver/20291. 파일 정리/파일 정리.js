8ed784da09caca8fa0d8a8d967023d3da7d00405
Yunjoo Cho <zooyaa@kakao.com>
2025-06-22 17:55:48 +0900

    [Silver III] Title: 파일 정리, Time: 636 ms, Memory: 64616 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.js
@@ -0,0 +1,28 @@
+const readline = require('readline');
+
+(async () => {
+    const rl = readline.createInterface({ input: process.stdin });
+    
+    const input = [];
+    for await (const line of rl) {
+        input.push(line.trim());
+        if (input.length === Number(input[0]) + 1) rl.close();
+    };
+    
+    const N = Number(input[0]);
+    const files = input.slice(1).map(file => file.split('.').pop());
+    
+    const map = new Map();
+    for (const file of files) {
+        if (map.has(file)) {
+            map.set(file, map.get(file) + 1);
+        } else {
+            map.set(file, 1);
+        };
+    }
+    
+    const result = Array.from(map.entries());
+    result.sort((a, b) => a[0].localeCompare(b[0]));
+    
+    console.log(result.map(file => file.join(' ')).join('\n'));
+})();
