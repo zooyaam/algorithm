d964c0b2dd9678f4bccbcf201ef1f1c55a62f065
Yunjoo Cho <zooyaa@kakao.com>
2026-01-30 23:07:12 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] result = new int[end_num - start_num + 1];
+        
+        for (int i = 0; i < result.length; i++) {
+            result[i] = i + start_num;
+        }
+        
+        return result;
+    }
+}
