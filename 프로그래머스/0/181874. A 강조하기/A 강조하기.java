be2638c91efdfc10821329b27aa73c79d8fc6e2d
Yunjoo Cho <zooyaa@kakao.com>
2026-02-20 21:00:20 +0900

    [level 0] Title: A 강조하기, Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        char[] arr = myString.toCharArray();
-        
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 'a') {
-                arr[i] = Character.toUpperCase(arr[i]);
-            } else if (arr[i] != 'A') {
-                arr[i] = Character.toLowerCase(arr[i]);
-            }
-        }
-        
-        return new String(arr);
+        return myString.toLowerCase().replace('a', 'A');
     }
 }
