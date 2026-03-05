36a0d6da31b13d7de7272693439d345f08f028fc
Yunjoo Cho <zooyaa@kakao.com>
2026-03-05 20:44:27 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        if (isOdd(a) && isOdd(b)) {
+            return a * a + b * b;
+        } else if (isOdd(a) || isOdd(b)) {
+            return 2 * (a + b);
+        } else {
+            return Math.abs(a - b);
+        }
+    }
+    
+    private boolean isOdd(int n) {
+        return n % 2 != 0;
+    }
+}
