de128cb3e1db2c90b51b645b505b2173fa1082cd
Yunjoo Cho <zooyaa@kakao.com>
2026-03-14 17:45:59 +0900

    [level 2] Title: 전화번호 목록, Time: 352.23 ms, Memory: 213 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,11 +1,18 @@
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
         
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) return false;
+        for (String num : phone_book) set.add(num);
+        
+        for (String num : set) {
+            for (int i = 1; i < num.length(); i++) {
+                String prefix = num.substring(0, i);
+                
+                if (set.contains(prefix)) return false;
+            }
         }
         
         return true;
