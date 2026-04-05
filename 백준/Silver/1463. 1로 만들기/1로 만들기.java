0930b05f1a4e43dd8074c9537b94c28661bd9d7c
Yunjoo Cho <zooyaa@kakao.com>
2026-04-05 11:09:57 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+        
+        int N = Integer.parseInt(br.readLine());        
+        
+        int[] dp = new int[N+1];
+        dp[1] = 0;
+        
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i-1] + 1;
+            
+            if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+        }
+        
+        System.out.print(dp[N]);
+    }
+}
